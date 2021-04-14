a7bede16a87fbcb956f9a42e10515f4c34a082eb
isabelSoares <isabel.srsoares@gmail.com>
2021-04-14 19:20:32 +0100

    Backend done for now!
    Co-authored-by: Rodrigo Sousa <Sousa99@users.noreply.github.com>

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -23,66 +23,106 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+app.get('/user/:user_id', (req, res) => {
+
+    let user_id = req.params['user_id'];
+    get_document('users', user_id).then((data) => {    
+        if (data == undefined) res.send("ERROR: User not found");
+        else res.send(JSON.stringify(load_user(data)));
+    });
+});
+
+
+app.get('/house/:house_id', (req, res) => {
+
+    let house_id = req.params['house_id'];
+    get_document('houses', house_id).then((data) => {    
+        if (data == undefined) res.send("ERROR: House not found");
+        else res.send(JSON.stringify(load_house(data)));
+    });
+});
+
+app.get('/floor/:floor_id', (req, res) => {
+
+    let floor_id = req.params['floor_id'];
+    get_document('floors', floor_id).then((data) => {    
+        if (data == undefined) res.send("ERROR: Floor not found");
+        else res.send(JSON.stringify(load_floor(data)));
+    });
+});
+
+app.get('/division/:division_id', (req, res) => {
+
+    let division_id = req.params['division_id'];
+    get_document('divisions', division_id).then((data) => {    
+        if (data == undefined) res.send("ERROR: Division not found");
+        else res.send(JSON.stringify(load_division(data)));
+    });
+});
+
+app.get('/device/:device_id', (req, res) => {
+
+    let device_id = req.params['device_id'];
+    get_document('devices', device_id).then((data) => {    
+        if (data == undefined) res.send("ERROR: Devices not found");
+        else res.send(JSON.stringify(load_device(data)));
+    });
+});
+
+app.get('/deviceType/:deviceType_id', (req, res) => {
+
+    let deviceType_id = req.params['deviceType_id'];
+    get_document('deviceTypes', deviceType_id).then((data) => {    
+        if (data == undefined) res.send("ERROR: DeviceType not found");
+        else res.send(JSON.stringify(load_device_type(data)));
+    });
+});
+
+app.get('/property/:property_id', (req, res) => {
+
+    let property_id = req.params['property_id'];
+    get_document('properties', property_id).then((data) => {
+        if (data == undefined) logger.info("ERROR: Property not found");
+        else console.log(load_property(data));
+    });
+});
+
+app.get('/property_value/:property_value_id', (req, res) => {
+
+    let property_value_id = req.params['property_value_id'];
+    get_document('propertyValues', property_value_id).then((data) => {
+        if (data == undefined) logger.info("ERROR: Property value not found");
+        else console.log(load_propertyValue(data));
+    });
+});
+
+app.get('/schedule/:schedule_id', (req, res) => {
+
+    let schedule_id = req.params['schedule_id'];
+    get_document('schedules', schedule_id).then((data) => {
+        if (data == undefined) logger.info("ERROR: Schedule not found");
+        else console.log(load_schedule(data));
+    });
+});
+
+app.get('/preference/:preference_id', (req, res) => {
+
+    let preference_id = req.params['preference_id'];
+    get_document('preferences', preference_id).then((data) => {
+        if (data == undefined) logger.info("ERROR: Preference not found");
+        else console.log(load_preference(data));
+    });
+});
+
+app.get('/value_history/:value_history_id', (req, res) => {
+
+    let value_history_id = req.params['value_history_id'];
+    get_document('valuesHistory', value_history_id).then((data) => {
+        if (data == undefined) res.send("ERROR: Value History not found");
+        else console.log(load_valueHistory(data));
+    });
+});
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`);
-});
-
-get_document('users', '3oj0mqDbQTzFawTi26UB').then((data) => {
-    if (data == undefined) logger.info("User not found");
-    else console.log(load_user(data));
-});
-
-get_document('houses', 'jYvO33oDVt5Yvjzr85zw').then((data) => {
-    if (data == undefined) logger.info("House not found");
-    else console.log(load_house(data));
-});
-
-get_document('floors', 'MqV7cGu4w8HtIM8uAGGx').then((data) => {
-    if (data == undefined) logger.info("Floor not found");
-    else console.log(load_floor(data));
-});
-
-get_document('divisions', '6HInKUX9PSvTNtUkodL9').then((data) => {
-    if (data == undefined) logger.info("Division not found");
-    else console.log(load_division(data));
-});
-
-get_document('devices', '3NBwsKco8OaoI35fApiF').then((data) => {
-    if (data == undefined) logger.info("Device not found");
-    else console.log(load_device(data));
-});
-
-get_document('deviceTypes', '5NCLAgikh9J0nMGbd4nv').then((data) => {
-    if (data == undefined) logger.info("Device type not found");
-    else console.log(load_device_type(data));
-});
-
-get_document('properties', 'XvHCO36UODyT6e7lxhDQ').then((data) => {
-    if (data == undefined) logger.info("Property not found");
-    else console.log(load_property(data));
-});
-
-get_document('properties', 'Psw565S3N2c51YxW6Yoq').then((data) => {
-    if (data == undefined) logger.info("Property not found");
-    else console.log(load_property(data));
-});
-
-get_document('propertyValues', 'FTQV1ayQgCpZPxFojRBm').then((data) => {
-    if (data == undefined) logger.info("Property value not found");
-    else console.log(load_propertyValue(data));
-});
-
-get_document('schedules', '7LFCa8e6Kjf8dPfBP3W4').then((data) => {
-    if (data == undefined) logger.info("Schedule not found");
-    else console.log(load_schedule(data));
-});
-
-get_document('preferences', '4oL7ztZJp2mw6qht6TQn').then((data) => {
-    if (data == undefined) logger.info("Preference not found");
-    else console.log(load_preference(data));
-});
-
-get_document('valuesHistory', 'AwkPtdpr0N2sYCUtmcQp').then((data) => {
-    if (data == undefined) logger.info("Value history not found");
-    else console.log(load_valueHistory(data));
 });
